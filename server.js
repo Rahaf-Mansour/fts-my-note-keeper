96ef6ffdd93d908c27a3d485006c9cefef0cfd56
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-09-12 20:53:37 +0300

    Create the notes router

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error)); // if there's an error when connecting to db
 db.once("open", () => console.log("Connected to the Database"));
+
+app.use(express.json());
+const notesRouter = require("./routes/notes");
+app.use("/notes/", notesRouter);
+
 app.listen(3000, () => console.log("Server is running"));
