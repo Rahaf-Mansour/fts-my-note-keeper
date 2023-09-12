a7667adb51d9b0665dffbb7e3f849b35f2e0ce58
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-09-12 20:47:28 +0300

    Connect to the mongo database

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express(); // to configure the server
+const mongoose = require("mongoose");
 
+// connect to the database
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error)); // if there's an error when connecting to db
+db.once("open", () => console.log("Connected to the Database"));
 app.listen(3000, () => console.log("Server is running"));
