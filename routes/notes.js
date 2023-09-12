f106575d18afdeed9d78a6b3e1496c9a139dee5a
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-09-12 21:07:05 +0300

    Getting started with notes router

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+// Getting all notes
+router.get("/", (req, res) => {});
+
+// Getting one note by id
+router.get("/:id", (req, res) => {});
+
+// Creating a new one
+router.post("/", (req, res) => {});
+
+// Updating one note by id
+router.patch("/:id", (req, res) => {});
+
+// Deleting one note by id
+router.delete("/:id", (req, res) => {});
 module.exports = router;
